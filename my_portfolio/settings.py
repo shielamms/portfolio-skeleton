cd9f0fef042ab0b97ac22596caeaf67e5831bd28
shielamms <shiela_mmioten@hotmail.com>
2025-03-15 13:38:18 +0000

    Revoke key and add Projects to Admin

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rqs7xor=jmhlj45clhlladij6h3_0@-=1w+vusz^x@xy$it()4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
